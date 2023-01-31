2d0965d40a72d107062598aadef42ecd9cc5068c
kode <kode.creer@gmail.com>
2023-01-31 13:26:01 -0700

    Initial Commit

---
+++ SimpleAIChatbot/SimpleAIChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAIChatbotApp.swift
+//  SimpleAIChatbot
+//
+//  Created by Kode Creer on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAIChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
